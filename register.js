d5bf978558a4bd391de08556945185a5d1f25c86
mehTro20 <justmondlisok@gmail.com>
2024-05-27 16:23:25 +0200

    I added javascrit juntionality to make the dashboard display data from local storage

--- register.js
+++ register.js
@@ -1,29 +1,71 @@
-document.getElementById('registrationForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    
-    var firstName = document.getElementById('firstName').value;
-    var lastName = document.getElementById('lastName').value;
-    var dateOfBirth = document.getElementById('dateOfBirth').value;
-    var email = document.getElementById('email').value;
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-   
+  var reg = document.getElementById("registrationForm");
 
-    var userData = {
-      
-      firstName: firstName,
-      lastName: lastName,
-      dateOfBirth: dateOfBirth,
-      email: email,
-      username: username,
-      password: password
-    };
+  reg && reg.addEventListener("submit",(event) =>{
+  event.preventDefault();
 
-    localStorage.setItem('userData', JSON.stringify(userData));
+  var firstName = document.getElementById("firstName").value;
+  var lastName = document.getElementById("lastName").value;
+  var dateOfBirth = document.getElementById("dateOfBirth").value;
+  var email = document.getElementById("email").value;
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  var userData = {
+    firstName: firstName,
+    lastName: lastName,
+    dateOfBirth: dateOfBirth,
+    email: email,
+    username: username,
+    password: password,
+  };
+
+  localStorage.setItem("userData", JSON.stringify(userData));
+  window.location.href = "dashboard.html";
+});
+
+// login form
+
+var btnLogin = document.getElementById("loginForm")
+
+btnLogin && btnLogin.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  var savedUser = JSON.parse(localStorage.getItem("userData"));
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  if (
+    savedUser &&
+    savedUser.username === username &&
+    savedUser.password === password
+  ) {
+    window.location.href = "dashboard.html";
+  } else {
+    alert("Invalid username or password");
+  }
+});
 
 
-    
-  
-    window.location.href = 'dashboard.html';
-  });
-  
+window.addEventListener("load", () => {
+  console.log('kjjjjjkkkj',window.location.pathname)
+  if (window.location.pathname.indexOf("dashboard.html") > -1) {
+    //map data from LS
+
+    var local = JSON.parse(localStorage.getItem("userData"));
+
+    if (local) {
+      console.log({local});
+      var userDataBody = document.getElementById("userDataBody");
+
+      userDataBody.innerHTML = `
+      <td>${local.firstName}</td>
+      <td>${local.lastName}</td>
+      <td>${local.dateOfBirth}</td>
+      <td>${local.email}</td>
+      <td>${local.username}</td>
+      <td>${local.password}</td>
+
+      `;
+    }
+  }
+});
